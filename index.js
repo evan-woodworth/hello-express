d3b618b5813f96438bd7cdb17debb2f957acb987
evan-woodworth <evan.woodworth@gmail.com>
2021-05-05 07:17:33 -0700

    index: add API route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Imports
 const express = require('express');
+const axios = require('axios');
 const app = express(); // instance of the app
 
 // Home route
@@ -12,8 +13,21 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/sei', (req, res) => {
+const sei = (req, res) => {
     res.send('SEI 412');
+}
+
+app.get('/sei', sei);
+
+app.get('/rockets', (req, res) => {
+    axios.get('https://api.spacexdata.com/v3/rockets')
+    .then(response => {
+        console.log(response.data);
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 });
 
 const PORT = process.env.PORT || 8000;
