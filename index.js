027feb8242b7143dbc38130543e89a71ec566cc0
evan-woodworth <evan.woodworth@gmail.com>
2021-05-05 08:03:15 -0700

    index: route 404

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const sei = (req, res) => {
-    res.send('SEI 412');
-}
+// const sei = (req, res) => {
+//     res.send('SEI 412');
+// }
 
-app.get('/sei', sei);
+// app.get('/sei', sei);
 
 app.get('/rockets', (req, res) => {
     axios.get('https://api.spacexdata.com/v3/rockets')
@@ -28,6 +28,29 @@ app.get('/rockets', (req, res) => {
     .catch(error => {
         console.log(error);
     })
+});
+
+app.get('/greet/:name', (req, res) => {
+    console.log(req.params);
+    res.send(`Hello ${req.params.name}`);
+});
+
+app.get('/github/:username', (req, res) => {
+    const {username} = req.params; //destructuring
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+        console.log(response.data);
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+// IMPORTANT NOTE: Make this the last route
+app.get('/*', (req, res) => {
+    console.log(req.params);
+    res.send('404 Not Found');
 });
 
 const PORT = process.env.PORT || 8000;
